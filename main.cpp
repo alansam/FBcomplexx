f6fbe1e0f09a22420c8ff5faded34eddcb4b2cdf
alansam <alansam@users.noreply.github.com>
2023-05-15 00:29:57 +0000

    Refactor - Separate implementation from definitions

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 //..+....|....+....|....+....|....+....|....+....|....+....|....+....|
 //  MARK: - Constants
-static constexpr bool LOG { true };
+static constexpr bool KLOG { true };
 
 //..+....|....+....|....+....|....+....|....+....|....+....|....+....|
 //  MARK: - Definitions
@@ -21,85 +21,64 @@ private:
   static constexpr bool LOG { true };
 
 public:
+  //  constructors - inline
   complexx(double a = 2.0, double b = 3.0)
   : real_ { a }, ima_ { b } {
-    if (LOG) { std::clog << "complexx::" << __func__ << "() default c'tor\n"; }
+    if (LOG) {
+      std::clog << "complexx::" << __func__
+                << "() default c'tor [@"
+                << std::hex << this << std::dec
+                << "]\n";
+    }
   }
 
-  // implement default c'tors etc. to prevent automaic generation.
+  // implement copy & move c'tors to prevent automatic generation.
   complexx(complexx const & that)
   : real_ { that.real_ }, ima_ { that.ima_ } {
-    if (LOG) { std::clog << "complexx::" << __func__ << "() copy c'tor\n"; }
+    if (LOG) {
+      std::clog << "complexx::" << __func__
+                << "() copy c'tor [@"
+                << std::hex
+                << this << ", @" << &that
+                << std::dec
+                << "]\n"; }
   }
   complexx(complexx const && that)
   : real_ { std::move(that.real_) }, ima_ { std::move(that.ima_) } {
-    if (LOG) { std::clog << "complexx::" << __func__ << "() move c'tor\n"; }
-  }
-  complexx & operator=(complexx const & that) {
-    if (LOG) { std::clog << "complexx::" << __func__ << "() copy\n"; }
-    real_ += that.real_;
-    ima_ += that.ima_;
-    return *this;
-  }
-  complexx & operator=(complexx && that) {
-    if (LOG) { std::clog << "complexx::" << __func__ << "() move\n"; }
-    real_ = std::move(that.real_);
-    ima_ = std::move(that.ima_);
-    return *this;
-  }
-  ~complexx() = default;
-
-  void rdcmplx(void) {
-    std::cout << "enter real & imaginary: ";
-    std::cin >> real_ >> ima_;
-    std::cout << std::endl;
-  }
-
-  void wrcmplx(void) {
-    if (LOG) { std::clog << "complexx::" << __func__ << "()\n"; }
-    std::cout << std::fixed << std::setprecision(3);
-    std::cout << std::setw(9)
-              << real_;
-    if (ima_ < 0.0) {
-      std::cout << " - i" << ::fabs(ima_) << '\n';
-    }
-    else {
-      std::cout << " + i" << ima_ << '\n';
+    if (LOG) {
+      std::clog << "complexx::" << __func__
+                << "() move c'tor [@"
+                << std::hex
+                << this << ", @" << &that
+                << std::dec
+                << "]\n";
     }
   }
 
-  complexx sum(complexx x, complexx y) const {
-    if (LOG) { std::clog << "complexx::" << __func__ << "(complexx,complexx)\n"; }
-    complexx temp;
-    temp.real_ = x.real_ + y.real_;
-    temp.ima_ = x.ima_ + y.ima_;
-    return temp;
+// implement copy & move operator=() to prevent automatic generation.
+  complexx & operator=(complexx const & that);
+  complexx & operator=(complexx && that);
+
+//  destructor - default
+#ifdef VIRT_DTOR  /* lest we forget */
+  virtual
+#endif
+  ~complexx() {
+    if (LOG) { std::clog << "complexx::" << __func__ << "() d'tor [@" << std::hex << this << std::dec << "]\n"; }
   }
 
-  complexx sum(complexx const & that) {
-    if (LOG) { std::clog << "complexx::" << __func__ << "(complexx)\n"; }
-    complexx temp;
-    temp.real_ = real_ + that.real_;
-    temp.ima_ = ima_ + that.ima_;
-    return temp;
-  }
+  void rdcmplx(void);
+  void wrcmplx(void);
 
-  complexx & operator+=(complexx const & that) {
-    if (LOG) { std::clog << "complexx::" << __func__ << "()\n"; }
-    real_ += that.real_;
-    ima_ += that.ima_;
-    return *this;
-  }
+  complexx sum(complexx x, complexx y) const;
+  complexx add(complexx const & that);
 
-  complexx & operator-=(complexx const & that) {
-    if (LOG) { std::clog << "complexx::" << __func__ << "()\n"; }
-    real_ -= that.real_;
-    ima_ -= that.ima_;
-    return *this;
-  }
+  complexx & operator+=(complexx const & that);
+  complexx & operator-=(complexx const & that);
 
   //  etc.
 
+  //  accessors (getters) & mutators (setters) - inline
   double real(void) const {
     return real_;
   }
@@ -116,59 +95,215 @@ public:
     return (ima_ = im);
   }
 
+  //  MARK: operator<<() - Friend function
   friend
-  std::ostream & operator<<(std::ostream & os, complexx const & that) {
-    if (LOG) { std::clog << "complexx::" << __func__ << "()\n"; }
-    os << std::fixed << std::setprecision(3);
-    os << std::setw(9)
-       << that.real_;
-    if (that.ima_ < 0.0) {
-      os << " - i" << ::fabs(that.ima_);
-    }
-    else {
-      os << " + i" << that.ima_;
-    }
-    return os;
-  }
+  std::ostream & operator<<(std::ostream & os, complexx const & that);
 
-  //  TODO: this doesn't seem logical, it needs too much knlowledge of the class
-  //  probably safer to read data into temp vars. and insert them into the
-  //  via the setter methods.
+  //  MARK: operator>>() - Friend function
   friend
-  std::istream & operator>>(std::istream & in, complexx & that) {
-    if (LOG) { std::clog << "complexx::" << __func__ << "()\n"; }
-    std::cin >> that.real_ >> that.ima_;
-    return in;
-  }
+  std::istream & operator>>(std::istream & in, complexx & that);
 
-  //  MARK: operator+() - Helper function
+  //  MARK: operator+() - Friend function
   friend
-  complexx operator+(complexx const & lft, complexx const & rgt) {
-    if (LOG) { std::clog << "complexx::" << __func__ << "(complexx,complexx)\n"; }
-    complexx temp;
-    temp.real_ = lft.real_ + rgt.real_;
-    temp.ima_ = lft.ima_ + rgt.ima_;
-    return temp;
-  }
-
-  //  MARK: operator-() - Helper function
+  complexx operator+(complexx const & lft, complexx const & rgt);
+  //  MARK: operator-() - Friend function
   friend
-  complexx operator-(complexx const & lft, complexx const & rgt) {
-    if (LOG) { std::clog << "complexx::" << __func__ << "(complexx,complexx)\n"; }
-    complexx temp;
-    temp.real_ = lft.real_ - rgt.real_;
-    temp.ima_ = lft.ima_ - rgt.ima_;
-    return temp;
-  }
+  complexx operator-(complexx const & lft, complexx const & rgt);
 };
 
 //  MARK: - Implementation
+//..+....|....+....|....+....|....+....|....+....|....+....|...
+//  MARK:  complexx::operator=() copy
+complexx & complexx::operator=(complexx const & that) {
+  if (LOG) {
+    std::clog << "complexx::" << __func__
+              << "() copy [@"
+              << std::hex
+              << this << ", @" << &that
+              << std::dec
+              << "]\n";
+          }
+  real_ += that.real_;
+  ima_ += that.ima_;
+  return *this;
+}
+//  MARK:  complexx::operator=() move
+complexx & complexx::operator=(complexx && that) {
+  if (LOG) {
+    std::clog << "complexx::" << __func__
+              << "() move [@"
+              << std::hex
+              << this << ", @" << &that
+              << std::dec
+              << "]\n";
+          }
+  real_ = std::move(that.real_);
+  ima_ = std::move(that.ima_);
+  return *this;
+}
+
+//  MARK:  complexx::rdcmplx()
+void complexx::rdcmplx(void) {
+  if (LOG) { std::clog << "complexx::" << __func__ << "() [@" << std::hex << this << std::dec << "]\n"; }
+  std::cout << "enter real & imaginary: ";
+  std::cin >> real_ >> ima_;
+  std::cout << std::endl;
+}
+
+//  MARK:  complexx::wrcmplx()
+void complexx::wrcmplx(void) {
+  if (LOG) { std::clog << "complexx::" << __func__ << "() [@" << std::hex << this << std::dec << "]\n"; }
+  std::cout << std::fixed << std::setprecision(3);
+  std::cout << std::setw(9)
+            << real_;
+  if (ima_ < 0.0) {
+    std::cout << " - i" << ::fabs(ima_) << '\n';
+  }
+  else {
+    std::cout << " + i" << ima_ << '\n';
+  }
+}
+
+//  MARK:  complexx::sum()
+complexx complexx::sum(complexx x, complexx y) const {
+  if (LOG) {
+    std::clog << "complexx::" << __func__
+              << "(complexx,complexx) [@"
+              << std::hex
+              << this << ", @" << &x << ", @" << &y
+              << std::dec
+              << "]\n";
+  }
+  complexx temp;
+  temp.real_ = x.real_ + y.real_;
+  temp.ima_ = x.ima_ + y.ima_;
+  return temp;
+}
+
+//  MARK:  complexx::add()
+complexx complexx::add(complexx const & that) {
+  if (LOG) {
+    std::clog << "complexx::" << __func__
+              << "(complexx) [@"
+              << std::hex
+              << this << ", @" << &that
+              << std::dec
+              << "]\n";
+          }
+  complexx temp;
+  temp.real_ = real_ + that.real_;
+  temp.ima_ = ima_ + that.ima_;
+  return temp;
+}
+
+//  MARK:  complexx::operator+=()
+complexx & complexx::operator+=(complexx const & that) {
+  if (LOG) {
+    std::clog << "complexx::" << __func__
+              << "() [@"
+              << std::hex
+              << this << ", @" << &that
+              << std::dec
+              << "]\n";
+          }
+  real_ += that.real_;
+  ima_ += that.ima_;
+  return *this;
+}
+
+//  MARK:  complexx::operator-=()
+complexx & complexx::operator-=(complexx const & that) {
+  if (LOG) {
+    std::clog << "complexx::" << __func__
+              << "() [@"
+              << std::hex
+              << this << ", @" << &that
+              << std::dec
+              << "]\n";
+          }
+  real_ -= that.real_;
+  ima_ -= that.ima_;
+  return *this;
+}
+
+//  MARK: operator+() - Friend function
+complexx operator+(complexx const & lft, complexx const & rgt) {
+  if (lft.LOG) {
+    std::clog << "complexx::" << __func__
+              << "(complexx,complexx) [@"
+              << std::hex
+              << &lft << ", @" << &rgt
+              << std::dec
+              << "]\n";
+  }
+  complexx temp;
+  temp.real_ = lft.real_ + rgt.real_;
+  temp.ima_ = lft.ima_ + rgt.ima_;
+  return temp;
+}
+
+//  MARK: operator-() - Friend function
+complexx operator-(complexx const & lft, complexx const & rgt) {
+  if (lft.LOG) {
+    std::clog << "complexx::" << __func__
+              << "(complexx,complexx) [@"
+              << std::hex
+              << &lft << ", @" << &rgt
+              << std::dec
+              << "]\n";
+          }
+  complexx temp;
+  temp.real_ = lft.real_ - rgt.real_;
+  temp.ima_ = lft.ima_ - rgt.ima_;
+  return temp;
+}
+
+//  MARK: operator<<() - Friend function
+std::ostream & operator<<(std::ostream & os, complexx const & that) {
+  if (that.LOG) {
+    std::clog << "complexx::" << __func__
+              << "() [@"
+              << std::hex
+              << &that
+              << std::dec
+              << "]\n";
+  }
+  os << std::fixed << std::setprecision(3);
+  os << std::setw(9)
+     << that.real_;
+  if (that.ima_ < 0.0) {
+    os << " - i" << ::fabs(that.ima_);
+  }
+  else {
+    os << " + i" << that.ima_;
+  }
+  return os;
+}
+
+//  MARK: operator>>() - Friend function
+//  TODO: this doesn't seem logical, it needs too much knlowledge of the class
+//  probably safer to read data into temp vars. and insert them into the
+//  via the setter methods.
+std::istream & operator>>(std::istream & in, complexx & that) {
+  if (that.LOG) {
+    std::clog << "complexx::" << __func__
+              << "() [@"
+              << std::hex
+              << &that
+              << std::dec
+              << "]\n";
+          }
+  std::cin >> that.real_ >> that.ima_;
+  return in;
+}
+
+
 //..+....|....+....|....+....|....+....|....+....|....+....|....+....|
 //  MARK: main()
 int main(int argc, char const * argv[]) {
   std::string dlm = "A: ....+....|....+....|....+....|....+....|....+....|";
   std::cout << dlm << '\n';
-  if (LOG) { std::clog << dlm << '\n'; }
+  if (KLOG) { std::clog << dlm << '\n'; }
   complexx p1(1.0, 1.0);
   complexx p2(-33.0, -11.0);
   complexx p3;
@@ -178,7 +313,7 @@ int main(int argc, char const * argv[]) {
 
   dlm = "B: ....+....|....+....|....+....|....+....|....+....|";
   std::cout << dlm << '\n';
-  if (LOG) { std::clog << dlm << '\n'; }
+  if (KLOG) { std::clog << dlm << '\n'; }
   std::cout << "sum(p1, p2)\n";
   p3 = p3.sum(p1, p2);
   std::cout << ":::\n";
@@ -189,10 +324,10 @@ int main(int argc, char const * argv[]) {
 
   dlm = "C: ....+....|....+....|....+....|....+....|....+....|";
   std::cout << dlm << '\n';
-  if (LOG) { std::clog << dlm << '\n'; }
-  std::cout << "p1.sum(p2)\n";
+  if (KLOG) { std::clog << dlm << '\n'; }
+  std::cout << "p1.add(p2)\n";
   p3 = complexx();
-  p3 = p1.sum(p2);
+  p3 = p1.add(p2);
   std::cout << ":::\n";
   p3.wrcmplx();
   std::cout << "}}} p1 " << p1 << ";\n"
@@ -201,7 +336,7 @@ int main(int argc, char const * argv[]) {
 
   dlm = "D: ....+....|....+....|....+....|....+....|....+....|";
   std::cout << dlm << '\n';
-  if (LOG) { std::clog << dlm << '\n'; }
+  if (KLOG) { std::clog << dlm << '\n'; }
   std::cout << "(p3 = p1 + p2)\n";
   std::cout << ":::\n";
   p3 = complexx();
@@ -212,7 +347,7 @@ int main(int argc, char const * argv[]) {
 
   dlm = "E: ....+....|....+....|....+....|....+....|....+....|";
   std::cout << dlm << '\n';
-  if (LOG) { std::clog << dlm << '\n'; }
+  if (KLOG) { std::clog << dlm << '\n'; }
   std::cout << "(p3 += p2)\n";
   std::cout << ":::\n";
   p3 = complexx();
@@ -223,7 +358,7 @@ int main(int argc, char const * argv[]) {
 
   dlm = "F: ....+....|....+....|....+....|....+....|....+....|";
   std::cout << dlm << '\n';
-  if (LOG) { std::clog << dlm << '\n'; }
+  if (KLOG) { std::clog << dlm << '\n'; }
   std::cout << "(p3 = p1 - p2)\n";
   std::cout << ":::\n";
   p3 = complexx();
@@ -234,7 +369,7 @@ int main(int argc, char const * argv[]) {
 
   dlm = "G: ....+....|....+....|....+....|....+....|....+....|";
   std::cout << dlm << '\n';
-  if (LOG) { std::clog << dlm << '\n'; }
+  if (KLOG) { std::clog << dlm << '\n'; }
   std::cout << "(p3 -= p2)\n";
   std::cout << ":::\n";
   p3 = complexx();
@@ -243,11 +378,22 @@ int main(int argc, char const * argv[]) {
             << "}}} p3 " << p3 << ";\n"
             << "}}} p3 " << (p3 -= p2) << ";\n";
 
-#ifdef INTERACTIVE
+  dlm = "H: ....+....|....+....|....+....|....+....|....+....|";
+  std::cout << dlm << '\n';
+  if (KLOG) { std::clog << dlm << '\n'; }
+  std::cout << "(p3 = p1 + p2 - p3)\n";
+  std::cout << ":::\n";
+  p3 = complexx();
+  std::cout << "}}} p1 " << p1 << ";\n"
+            << "}}} p2 " << p2 << ";\n"
+            << "}}} p3 " << p3 << ";\n"
+            << "}}} p3 " << (p3 = p1 + p2 - p3) << ";\n";
+
+  #ifdef INTERACTIVE
   {
-    dlm = "H: ....+....|....+....|....+....|....+....|....+....|";
+    dlm = "I: ....+....|....+....|....+....|....+....|....+....|";
     std::cout << dlm << '\n';
-    if (LOG) { std::clog << dlm << '\n'; }
+    if (KLOG) { std::clog << dlm << '\n'; }
     std::cout << "std::cin >> t1 >> t2\n";
     std::cout << ":::\n";
     p3 = p1;
@@ -264,10 +410,10 @@ int main(int argc, char const * argv[]) {
 
 #ifdef INTERACTIVE
   {
-    dlm = "I: ....+....|....+....|....+....|....+....|....+....|";
+    dlm = "J: ....+....|....+....|....+....|....+....|....+....|";
     std::cout << dlm << '\n';
-    if (LOG) { std::clog << dlm << '\n'; }
-    std::cout << "std::cin >> r3\n";
+    if (KLOG) { std::clog << dlm << '\n'; }
+    std::cout << "std::cin >> p3\n";
     std::cout << ":::\n";
     std::cout << "enter real & imaginary: ";
     std::cin >> p3;
@@ -277,7 +423,7 @@ int main(int argc, char const * argv[]) {
 #endif
 
   std::cout << "Done ...\n";
-  if (LOG) { std::clog << "Done ...\n"; }
+  if (KLOG) { std::clog << "Done ...\n"; }
 
   return 0;
 }
